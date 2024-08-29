395f3b24ed2b4c357c670f1b41d189281ebdadc5
Alejandro de Pablos Lopez <alejandro.depablos@sngular.com>
2024-08-29 15:16:25 -0400

    refactor: reorganize types and update inversify configuration
    - Create src/types/ directory to centralize type definitions
    - Update TYPES to DI_TYPES imports in inversify.config.ts
    - Improve organization of dependency injection types

--- src/inversify.config.ts
+++ src/inversify.config.ts
@@ -1,32 +1,33 @@
 import 'reflect-metadata';
-import {Container} from 'inversify';
-import {TYPES} from './types';
-import {UserService} from './services/userService';
-import {HouseholdService} from './services/householdService';
-import {ExpenseService} from './services/expenseService';
-import {CategoryService} from './services/categoryService';
-import {SubcategoryService} from './services/subcategoryService';
-import {NotificationService} from './services/external/notificationService';
-import {UserController} from './controllers/userController';
-import {HouseholdController} from './controllers/householdController';
-import {ExpenseController} from './controllers/expenseController';
-import {CategoryController} from './controllers/categoryController';
-import {SubcategoryController} from './controllers/subcategoryController';
+import { Container } from 'inversify';
+
+import { CategoryController } from './controllers/categoryController';
+import { ExpenseController } from './controllers/expenseController';
+import { HouseholdController } from './controllers/householdController';
+import { SubcategoryController } from './controllers/subcategoryController';
+import { UserController } from './controllers/userController';
+import { CategoryService } from './services/categoryService';
+import { ExpenseService } from './services/expenseService';
+import { NotificationService } from './services/external/notificationService';
+import { HouseholdService } from './services/householdService';
+import { SubcategoryService } from './services/subcategoryService';
+import { UserService } from './services/userService';
+import { DI_TYPES } from './types/di';
 
 const container = new Container();
 
 // Bind your services and controllers here
-container.bind<UserService>(TYPES.UserService).to(UserService);
-container.bind<HouseholdService>(TYPES.HouseholdService).to(HouseholdService);
-container.bind<ExpenseService>(TYPES.ExpenseService).to(ExpenseService);
-container.bind<CategoryService>(TYPES.CategoryService).to(CategoryService);
-container.bind<SubcategoryService>(TYPES.SubcategoryService).to(SubcategoryService);
-container.bind<NotificationService>(TYPES.NotificationService).to(NotificationService);
+container.bind<UserService>(DI_TYPES.UserService).to(UserService);
+container.bind<HouseholdService>(DI_TYPES.HouseholdService).to(HouseholdService);
+container.bind<ExpenseService>(DI_TYPES.ExpenseService).to(ExpenseService);
+container.bind<CategoryService>(DI_TYPES.CategoryService).to(CategoryService);
+container.bind<SubcategoryService>(DI_TYPES.SubcategoryService).to(SubcategoryService);
+container.bind<NotificationService>(DI_TYPES.NotificationService).to(NotificationService);
 
-container.bind<UserController>(TYPES.UserController).to(UserController);
-container.bind<HouseholdController>(TYPES.HouseholdController).to(HouseholdController);
-container.bind<ExpenseController>(TYPES.ExpenseController).to(ExpenseController);
-container.bind<CategoryController>(TYPES.CategoryController).to(CategoryController);
-container.bind<SubcategoryController>(TYPES.SubcategoryController).to(SubcategoryController);
+container.bind<UserController>(DI_TYPES.UserController).to(UserController);
+container.bind<HouseholdController>(DI_TYPES.HouseholdController).to(HouseholdController);
+container.bind<ExpenseController>(DI_TYPES.ExpenseController).to(ExpenseController);
+container.bind<CategoryController>(DI_TYPES.CategoryController).to(CategoryController);
+container.bind<SubcategoryController>(DI_TYPES.SubcategoryController).to(SubcategoryController);
 
-export {container};
+export { container };
---
+++ src/types/di.ts
@@ -0,0 +1,18 @@
+export const DI_TYPES = {
+  // Services
+  CategoryService: Symbol.for('CategoryService'),
+  SubcategoryService: Symbol.for('SubcategoryService'),
+  ExpenseService: Symbol.for('ExpenseService'),
+  HouseholdService: Symbol.for('HouseholdService'),
+  UserService: Symbol.for('UserService'),
+  NotificationService: Symbol.for('NotificationService'),
+  // Add other services here as needed
+
+  // Controllers
+  CategoryController: Symbol.for('CategoryController'),
+  SubcategoryController: Symbol.for('SubcategoryController'),
+  ExpenseController: Symbol.for('ExpenseController'),
+  HouseholdController: Symbol.for('HouseholdController'),
+  UserController: Symbol.for('UserController'),
+  // Add other controllers here as needed
+};
