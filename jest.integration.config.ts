f60b6df52dfa4f6e4fe2c1499b98922d4ef4cb42
Alejandro de Pablos Lopez <alejandro.depablos@sngular.com>
2024-10-24 10:20:45 -0400

    Refactor Jest configuration
    Centralized Jest configurations with a base config and updated unit and integration test settings.

--- jest.integration.config.ts
+++ jest.integration.config.ts
@@ -1,50 +1,14 @@
 import type { Config } from '@jest/types';
 
-import logger from './src/config/logger';
-
-// Configura el logger para los tests
-logger.level = 'debug';
+import baseConfig from './jest.config.base';
 
 const config: Config.InitialOptions = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  testMatch: [
-    '**/tests/integration/**/*.test.(ts|js)',
-    '**/tests/integration/?(*.)+(spec|test).[tj]s?(x)',
-  ],
-  transform: {
-    '^.+\\.tsx?$': [
-      'ts-jest',
-      {
-        tsconfig: 'tsconfig.test.json',
-        diagnostics: true,
-        isolatedModules: false,
-      },
-    ],
-  },
+  ...baseConfig,
+  displayName: 'integration',
+  testMatch: ['**/tests/integration/**/*.test.(ts|js)'],
   setupFilesAfterEnv: ['<rootDir>/tests/integration/setup/jest.setup.ts'],
-  detectOpenHandles: true,
-  moduleDirectories: ['node_modules', 'src'],
-  roots: ['<rootDir>/src', '<rootDir>/tests/integration'],
   coverageDirectory: 'coverage/integration',
-  collectCoverageFrom: ['src/**/*.{ts,tsx}', '!src/**/*.d.ts'],
-  verbose: true,
-  coverageThreshold: {
-    global: {
-      statements: 80,
-      branches: 70,
-      functions: 80,
-      lines: 80,
-    },
-  },
-  coveragePathIgnorePatterns: [
-    '/node_modules/',
-    'src/swagger.ts',
-    'src/config/*',
-    'src/index.ts',
-    'src/server.ts',
-  ],
+  detectOpenHandles: true,
   reporters: [
     'default',
     [
--- jest.unit.config.ts
+++ jest.unit.config.ts
@@ -1,34 +1,14 @@
 import type { Config } from '@jest/types';
 
+import baseConfig from './jest.config.base';
+
 const config: Config.InitialOptions = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  testMatch: ['**/tests/unit/**/*.test.(ts|js)', '**/tests/unit/?(*.)+(spec|test).[tj]s?(x)'],
-  transform: {
-    '^.+\\.tsx?$': [
-      'ts-jest',
-      {
-        tsconfig: 'tsconfig.json',
-        diagnostics: false,
-        isolatedModules: true,
-      },
-    ],
-  },
+  ...baseConfig,
+  displayName: 'unit',
+  testMatch: ['**/tests/unit/**/*.test.(ts|js)'],
   setupFilesAfterEnv: ['<rootDir>/tests/unit/setup/jest.setup.ts'],
-  moduleDirectories: ['node_modules', 'src'],
-  roots: ['<rootDir>/src', '<rootDir>/tests/unit'],
   coverageDirectory: 'coverage/unit',
   collectCoverageFrom: ['src/**/*.{ts,tsx}', '!src/**/*.d.ts'],
-  verbose: true,
-  coverageThreshold: {
-    global: {
-      statements: 80,
-      branches: 70,
-      functions: 80,
-      lines: 80,
-    },
-  },
   coveragePathIgnorePatterns: [
     '/node_modules/',
     'src/swagger.ts',
